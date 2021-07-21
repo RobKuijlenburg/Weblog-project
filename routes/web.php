c7a31c4ac516986efb78731611c3a62242d67dca
RobKuijlenburg <58665690+RobKuijlenburg@users.noreply.github.com>
2021-07-21 15:59:49 +0200

    added blades

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArticlesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/articles', [ArticlesController::class, 'index'])
+    ->name('articles.index');
+
+
+Route::get('/articles/create', [ArticlesController::class, 'create'])
+    ->name('articles.create');
+
+Route::post('/articles', [ArticlesController::class, 'store'])
+    ->name('articles.store');
+
+Route::get('/articles/{article}/edit', [ArticlesController::class, 'edit'])
+    ->name('articles.edit');
+
+Route::put('/articles/{article}', [ArticlesController::class, 'update'])
+    ->name('articles.update');
+
+Route::delete('/articles/{article}', [ArticlesController::class, 'destroy'])
+    ->name('articles.destroy');
+
+Route::redirect('/', '/articles');
