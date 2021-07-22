cf2ad07b41a14cc0f01e294096b9e4b6cba90d0d
RobKuijlenburg <58665690+RobKuijlenburg@users.noreply.github.com>
2021-07-22 12:35:12 +0200

    models aangemaakt

--- database/migrations/2021_07_22_092531_create_articles_table.php
+++ database/migrations/2021_07_22_092531_create_articles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateArticlesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('author')->default(false);
-            $table->boolean('subscribed')->default(false);
+            $table->foreignId('user_id');
+            $table->string('title');
+            $table->string('slug');
+            $table->string('excerpt');
+            $table->string('body');
+            $table->string('img');
             $table->boolean('premium')->default(false);
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -34,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('articles');
     }
 }
