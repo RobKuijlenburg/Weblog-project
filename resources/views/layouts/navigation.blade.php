fab5737953b766d8094af681a34684f07475599e
RobKuijlenburg <58665690+RobKuijlenburg@users.noreply.github.com>
2021-07-28 14:23:02 +0200

    Update nav blade to include register

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -36,6 +36,9 @@
                     <x-nav-link :href="route('login')" :active="request()->routeIs('login')">
                         {{ __('Login') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('register')" :active="request()->routeIs('register')">
+                        {{ __('Register') }}
+                    </x-nav-link>
                     @endauth
                 </nav>
   
